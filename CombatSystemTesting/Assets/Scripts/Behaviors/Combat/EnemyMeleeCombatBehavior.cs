0fec55b0c6ef9da64dea6df35f5f73b2ea3cd632
Flashcaptain <36227316+Flashcaptain@users.noreply.github.com>
2019-11-12 16:44:57 +0100

    push

--- CombatSystemTesting/Assets/Scripts/Behaviors/Combat/EnemyMeleeCombatBehavior.cs
+++ CombatSystemTesting/Assets/Scripts/Behaviors/Combat/EnemyMeleeCombatBehavior.cs
@@ -20,8 +20,25 @@ public class EnemyMeleeCombatBehavior : CombatBehavior
     private int _bashChance;
 
     [SerializeField]
-    [Range(0, 10)]
+    [Range(0, 5)]
     private float _reactionTime;
+
+    private BlockEnum _targetStance;
+
+    private void Start()
+    {
+        AttackLoop();
+    }
+
+    private void AttackLoop()
+    {
+        if ((_actor._currentTarget._blockEnum != BlockEnum.None && Vector3.Distance(_actor._currentTarget.transform.position, _actor.transform.position) < _targetRange) || Vector3.Distance(_actor._currentTarget.transform.position, _actor.transform.position) < _weapon._range)
+        {
+                //switch to random block enum
+                _actor._attack.MeleeAttack(_weapon, _crosshairManager);
+        }
+        Invoke("AttackLoop",1);
+    }
 
     private void Update()
     {
@@ -31,11 +48,20 @@ public class EnemyMeleeCombatBehavior : CombatBehavior
             {
                 _crosshairManager.ToggleState(true);
             }
-            _actor._blockEnum = _crosshairManager.GetBlockDirection(0, 0, this.transform);
-            if (false)//when attacks
+
+            if (_targetStance != _actor._currentTarget._blockEnum)
             {
-                _actor._attack.MeleeAttack(_weapon, _crosshairManager);
+                _targetStance = _actor._currentTarget._blockEnum;
+                StopAllCoroutines();
+
+                StartCoroutine(SwitchBlockDirection(_targetStance, Random.Range(0,_reactionTime)));
+
+                if ((_targetStance != BlockEnum.Bottom && PercentChance(_attackChance)) || (_targetStance == BlockEnum.Bottom && PercentChance(_bashChance)))
+                {
+                    _actor._attack.MeleeAttack(_weapon, _crosshairManager);
+                }
             }
+            _actor._blockEnum = _crosshairManager.GetBlockDirection(0, 0, this.transform);
             return;
         }
         if (_crosshairManager._isActive)
@@ -46,6 +72,36 @@ public class EnemyMeleeCombatBehavior : CombatBehavior
 
 
 
+    }
+
+    private IEnumerator SwitchBlockDirection(BlockEnum blockEnum, float switchTime)
+    {
+        yield return new WaitForSeconds(switchTime);
+
+        switch (blockEnum)
+        {
+            case BlockEnum.Top:
+                _actor._blockEnum = _crosshairManager.GetBlockDirection(0, 1, this.transform);
+                break;
+            case BlockEnum.Left:
+                _actor._blockEnum = _crosshairManager.GetBlockDirection(1, 0, this.transform);
+                break;
+            case BlockEnum.Right:
+                _actor._blockEnum = _crosshairManager.GetBlockDirection(-1, 0, this.transform);
+                break;
+            case BlockEnum.Bottom:
+                _actor._blockEnum = _crosshairManager.GetBlockDirection(0, -1, this.transform);
+                break;
+        }
+    }
+
+    private bool PercentChance(int Percent)
+    {
+        if (Random.Range(0,100) < Percent)
+        {
+            return true;
+        }
+        return false;
     }
 }
 
